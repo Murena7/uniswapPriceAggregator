bba344c121904ccff692550b6e3610c6bb0c4fb8
Andrei <>
2021-05-29 03:43:16 +0300

    Implement work with arr of token fix

--- scripts/deploy-test1.ts
+++ scripts/deploy-test1.ts
@@ -24,7 +24,11 @@ async function main() {
     const pairAddress = await deployedContract.getCurrentPrice('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', '0xe9e7cea3dedca5984780bafc599bd69add087d56');
     console.log('Result', buildBalanceTransformer(new BigNumber(pairAddress[0].toString()), pairAddress[2]).toString(), pairAddress[1], pairAddress[2]);
     const pairAddressesArr = await deployedContract.getCurrentPriceArr(['0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', '0xe9e7cea3dedca5984780bafc599bd69add087d56'], ['0xe9e7cea3dedca5984780bafc599bd69add087d56', '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c']);
-    console.log('Arr', pairAddressesArr)
+
+    for (let i = 0; i < pairAddressesArr[0].length; i++ ) {
+        console.log('Result', i, buildBalanceTransformer(new BigNumber(pairAddressesArr[0][i].toString()), pairAddressesArr[2][i]).toString(), pairAddressesArr[1][i], pairAddressesArr[2][i]);
+    }
+
     console.log("Token address:", deployedContract.address);
 }
 
